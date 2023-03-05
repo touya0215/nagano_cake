e0af310b63a0715907c1600d1cf19f1ff63fa5df
touya0215 <110541062+touya0215@users.noreply.github.com>
2023-03-05 23:31:46 +0000

    db作成

--- db/migrate/20230305133332_create_publics.rb
+++ db/migrate/20230305133332_create_publics.rb
@@ -1,7 +1,7 @@
 class CreatePublics < ActiveRecord::Migration[6.1]
   def change
     create_table :publics do |t|
-
+      t.string :name
       t.timestamps
     end
   end
--- db/migrate/20230305133446_create_admins.rb
+++ db/migrate/20230305133446_create_admins.rb
@@ -1,7 +1,9 @@
 class CreateAdmins < ActiveRecord::Migration[6.1]
   def change
     create_table :admins do |t|
-
+      t.string :name
+      t.string :title
+      t.string :body
       t.timestamps
     end
   end
