eee22b58b5ad7a62c879d91fbc71319dd6a9a71f
touya0215 <110541062+touya0215@users.noreply.github.com>
2023-05-06 08:56:06 +0000

    会員一覧

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,8 +10,5 @@ class ApplicationController < ActionController::Base
   end
   
   protected
-  def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:last_name, :first_name, :last_name_kama, :first_name_kana, :postal_code, :address, :telephone_number])
-  end
 
 end
