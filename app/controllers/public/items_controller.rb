0568bfcef47f2d8b92e844cafad3589cd6c586f3
touya0215 <110541062+touya0215@users.noreply.github.com>
2023-05-21 15:15:22 +0000

    商品一覧

--- app/controllers/public/items_controller.rb
+++ app/controllers/public/items_controller.rb
@@ -1,7 +1,13 @@
 class Public::ItemsController < ApplicationController
   def index
+    @item = Item.new
+    @items = Item.all
   end
 
   def show
+    @item = Item.new
+    @items = Item.find(params[:id])
   end
+  
+  
 end
