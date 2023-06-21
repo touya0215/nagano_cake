9fe0ccfe2bf8d96367f4083ccb4eafc10fe368c8
touya0215 <110541062+touya0215@users.noreply.github.com>
2023-06-21 14:17:12 +0000

    注文情報確認

--- app/models/order.rb
+++ app/models/order.rb
@@ -2,10 +2,10 @@ class Order < ApplicationRecord
   belongs_to :item
   belongs_to :customer
   has_many :order_details
-  
 
-  enum payment_method: {"クレジットカード": 0,"銀行振込": 1}
 
-  
+  enum payment_method: { credit_card: 0, transfer: 1 }
+
+
 
 end
