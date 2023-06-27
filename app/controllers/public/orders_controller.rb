591d3ac4823d8aa90e5fee95ebf6e8e0a9187648
touya0215 <110541062+touya0215@users.noreply.github.com>
2023-06-27 17:22:53 +0000

    注文履歴詳細

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -13,6 +13,9 @@ class Public::OrdersController < ApplicationController
   end
 
   def show
+    @order = Order.find(params[:id])
+    @order.shipping_cost = 800
+    @order_details = @order.order_details
   end
 
   def create
