f9380eb774e811fb99c4f828f290888f0769659e
touya0215 <110541062+touya0215@users.noreply.github.com>
2023-06-28 08:28:28 +0000

    注文履歴詳細

--- app/models/order_detail.rb
+++ app/models/order_detail.rb
@@ -1,4 +1,13 @@
 class OrderDetail < ApplicationRecord
   belongs_to :order
   belongs_to :item
+
+  def sum_total
+    shipping_cost + total_payment
+  end
+  
+  def sum_of_price
+    item.texin_price * amount
+  end
+
 end
