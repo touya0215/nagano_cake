7db65556781c431a0952e74b27877c260f46da0f
touya0215 <110541062+touya0215@users.noreply.github.com>
2023-07-02 14:00:58 +0000

    注文履歴詳細

--- app/controllers/admins/orders_controller.rb
+++ app/controllers/admins/orders_controller.rb
@@ -1,5 +1,7 @@
 class Admins::OrdersController < ApplicationController
   def show
     @order = Order.find(params[:id])
+    @order.shipping_cost = 800
+    @order_details = @order.order_details
   end
 end
